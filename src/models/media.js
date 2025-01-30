510270df3c4e1d29169ee13d9e3e224bc6fed36b
Anurag Dey <iamanuragdey005@gmail.com>
2025-01-30 21:18:16 +0530

    Fix Media Model as per Business Logic

--- src/models/media.js
+++ src/models/media.js
@@ -17,7 +17,7 @@ const Media = db.define('media', {
     },
     productId: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
             model: 'Products',
             key: 'id'
@@ -25,7 +25,7 @@ const Media = db.define('media', {
     },
     colorId: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
             model: 'colors',
             key: 'id'
